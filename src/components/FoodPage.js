1212b66ab039d7b02fc9f6a82bf8bcca752e5c07
Thomas Nguyen <TMNguyen2@student.fullsail.edu>
2022-10-18 00:31:24 -0400

    added routes to backend for insert food, meal, day

--- src/components/FoodPage.js
+++ src/components/FoodPage.js
@@ -7,6 +7,7 @@ import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
 import DatePicker from "./DatePicker";
 import FoodSearchForm from "./FoodSearchForm";
+import Axios from "axios";
 
 function FoodPage() {
   const [searching, setSearching] = useState(false);
@@ -16,11 +17,20 @@ function FoodPage() {
   const [pageNumber, setPageNumber] = useState([1]);
   const [loading, setLoading] = useState(false);
   const [selecting, setSelecting] = useState(false);
-  const [foodItem, setFoodItem] = useState([
+
+  const [userId, setUserId] = useState(1);
+  const [dayId, setDayId] = useState(1);
+  const [mealId, setMealId] = useState(1);
+
+  const [selectedFood, setSelectedFood] = useState([
     { servingSize: 0, foodNutrients: { value: 0 } },
   ]);
-  const [servingSize, setServingSize] = useState(0);
+  const [foodName, setFoodName] = useState("");
+  const [serving, setServing] = useState(0);
   const [calories, setCalories] = useState(0);
+  const [carbs, setCarbs] = useState(0);
+  const [protein, setProtein] = useState(0);
+  const [fat, setFat] = useState(0);
 
   const getCurrentDate = () => {
     const current = new Date();
@@ -72,25 +82,118 @@ function FoodPage() {
   };
 
   const handleSelectFood = (item) => {
-    setFoodItem(item);
+    setSelectedFood(item);
+    setFoodName(item.description);
     setSelecting(true);
     setSearchResults([]);
   };
 
   const handleServingSizeChange = (event) => {
-    setServingSize(event.target.value);
+    if (event.target.value === undefined) {
+      return;
+    }
+    setServing(parseInt(event.target.value));
 
-    for (let i = 0; i < foodItem.foodNutrients.length; i++) {
-      if (foodItem.foodNutrients[i].nutrientId === 1008) {
+    for (let i = 0; i < selectedFood.foodNutrients.length; i++) {
+      if (selectedFood.foodNutrients[i].nutrientId === 1008) {
         setCalories(
-          Math.round(event.target.value * foodItem.foodNutrients[i].value) / 100
+          Math.round(event.target.value * selectedFood.foodNutrients[i].value) /
+            100
         );
-        break;
+      }
+      if (selectedFood.foodNutrients[i].nutrientId === 1005) {
+        setCarbs(
+          Math.round(event.target.value * selectedFood.foodNutrients[i].value) /
+            100
+        );
+      }
+      if (selectedFood.foodNutrients[i].nutrientId === 1003) {
+        setProtein(
+          Math.round(event.target.value * selectedFood.foodNutrients[i].value) /
+            100
+        );
+      }
+      if (selectedFood.foodNutrients[i].nutrientId === 1004) {
+        setFat(
+          Math.round(event.target.value * selectedFood.foodNutrients[i].value) /
+            100
+        );
       }
     }
   };
 
-  const handleAddFood = async () => {};
+  const handleAddFood = async (event) => {
+    event.preventDefault();
+    /* await getDay(); */
+    /* await getMeal(); */
+    await addFood();
+    console.log("handle add food done");
+  };
+
+  const getDay = async (event) => {
+    await Axios.post("http://localhost:4000/api/get/day", {
+      userId: userId,
+      date: date,
+    })
+      .then((response) => {
+        if (response.data.error === true) {
+          console.log(response.data.message);
+        } else {
+          // got day id
+          setDayId(response.data.dayId.dayId);
+          console.log("got day");
+        }
+      })
+      .catch((error) => {
+        //if (error.code === "ERR_NETWORK")
+        console.log(error);
+      });
+  };
+
+  const getMeal = async (event) => {
+    await Axios.post("http://localhost:4000/api/get/meal", {
+      dayId: dayId,
+      meal: meal,
+    })
+      .then((response) => {
+        if (response.data.error === true) {
+          console.log(response.data.message);
+        } else {
+          // got meal id
+          setMealId(response.data.mealId.mealId);
+          console.log("got meal!");
+        }
+      })
+      .catch((error) => {
+        //if (error.code === "ERR_NETWORK")
+        console.log(error);
+      });
+  };
+
+  const addFood = async (event) => {
+    await Axios.post("http://localhost:4000/api/add/food", {
+      userId: userId,
+      foodName: foodName,
+      serving: serving,
+      calories: calories,
+      carbs: carbs,
+      protein: protein,
+      fat: fat,
+      mealId: mealId,
+    })
+      .then((response) => {
+        if (response.data.error === true) {
+          console.log(response.data.message);
+        } else {
+          // food add successful
+          console.log("food done!");
+        }
+      })
+      .catch((error) => {
+        //if (error.code === "ERR_NETWORK")
+        console.log(error);
+      });
+  };
 
   return (
     <>
@@ -112,6 +215,7 @@ function FoodPage() {
                 className="mt-2"
                 variant="dark"
                 id="dropdown-basic"
+                aria-required
               >
                 {meal}
               </Dropdown.Toggle>
@@ -134,11 +238,10 @@ function FoodPage() {
         <Container className="mt-3 mb-3 w-25 d-flex justify-content-center text-center">
           <div className="w-100">
             <FoodSearchForm
-              getFoodResults={handleFoodSubmit}
+              handleFoodSubmit={handleFoodSubmit}
               foodQuery={foodQuery}
               setFoodQuery={setFoodQuery}
               loading={loading}
-              setSearchResults={setSearchResults}
               handleClear={handleClear}
             />
           </div>
@@ -217,29 +320,29 @@ function FoodPage() {
         </div>
       )}
       {selecting && (
-        <Card key={foodItem.fdcId} className="text-center">
-          <Card.Header>{foodItem.description}</Card.Header>
+        <Card key={selectedFood.fdcId} className="text-center">
+          <Card.Header>{selectedFood.description}</Card.Header>
           <Card.Body>
             <Card.Text>
-              Id: {foodItem.fdcId}
+              Id: {selectedFood.fdcId}
               <br />
-              {foodItem.brandOwner && (
+              {selectedFood.brandOwner && (
                 <span>
-                  Brand: {foodItem.brandOwner}
+                  Brand: {selectedFood.brandOwner}
                   <br />
                 </span>
               )}
-              {foodItem.packageWeight && (
+              {selectedFood.packageWeight && (
                 <span>
-                  Package Weight: {foodItem.packageWeight}
+                  Package Weight: {selectedFood.packageWeight}
                   <br />
                 </span>
               )}
               <span>
-                {foodItem.servingSize
+                {selectedFood.servingSize
                   ? `Serving Size: ${
-                      Math.round(foodItem.servingSize * 100) / 100
-                    } ${foodItem.servingSizeUnit}`
+                      Math.round(selectedFood.servingSize * 100) / 100
+                    } ${selectedFood.servingSizeUnit}`
                   : "Serving Size: 100 g"}
               </span>
             </Card.Text>
@@ -252,7 +355,7 @@ function FoodPage() {
                 </tr>
               </thead>
               <tbody>
-                {foodItem.foodNutrients
+                {selectedFood.foodNutrients
                   .filter(
                     (nutrient) =>
                       nutrient.nutrientId === 1003 ||
@@ -264,9 +367,10 @@ function FoodPage() {
                     <tr key={nutrient.nutrientId}>
                       <td>{nutrient.nutrientName}</td>
                       <td>
-                        {foodItem.servingSize
-                          ? Math.round(nutrient.value * foodItem.servingSize) /
-                            100
+                        {selectedFood.servingSize
+                          ? Math.round(
+                              nutrient.value * selectedFood.servingSize
+                            ) / 100
                           : nutrient.value}{" "}
                         {nutrient.unitName}
                       </td>
@@ -279,7 +383,7 @@ function FoodPage() {
                 <Form.Label>Your serving size (G):</Form.Label>
                 <Form.Control
                   type="number"
-                  value={servingSize}
+                  value={serving}
                   onChange={handleServingSizeChange}
                   required
                 />
@@ -288,7 +392,7 @@ function FoodPage() {
                 <Form.Label>Calories:</Form.Label>
                 <Form.Control type="number" value={calories} disabled />
               </Form.Group>
-              <Button variant="dark" type="submit" onClick={handleAddFood}>
+              <Button variant="dark" type="submit">
                 Add Food
               </Button>
             </Form>
